057a68ec689c3e57556bc8348aad2340a70a6c58
hModule =/ <60050784+x86senniral@users.noreply.github.com>
2025-02-15 23:29:11 -0500

    Delete req.py

--- req.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://localhost:5000/set_alert"
-inp = input("Data to send: ")
-data = {"event": inp} 
-response = requests.post(url, json=data)
-
-print(response.json()) 
